8212a48566c1711766f1016cbce9291803b08991
Jisu An <ajs7270@image.cau.ac.kr>
2017-04-15 02:34:25 +0900

    fixed test

--- src/test.java
+++ src/test.java
@@ -1,11 +1,19 @@
+/**
+ * Created by 5p on 2017-04-14.
+ */
 
 public class test {
 
     public static void main(String[] args) {
 
-        ProductList newList=new ProductList();
+        ProductList newList = new ProductList();
+
         newList.displayProductList("productlist-norm.data");
         newList.displayProductList("productlist-norm1.data");
         newList.displayProductList("productlist-norm2.data");
     }
 }
+
+
+
+
